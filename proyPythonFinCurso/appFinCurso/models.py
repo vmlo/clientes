caea43f7ba6826112012a6eb714ca7c2ec54bdfd
EP <my.dev.challenges@gmail.com>
2024-06-29 21:25:18 +0200

    clase DatosTipo en fichero models.py

--- proyPythonFinCurso/appFinCurso/models.py
+++ proyPythonFinCurso/appFinCurso/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import cx_Oracle
 
-# Create your models here.
+class DatosTipo:
+    def __init__(self):
+        self.connection = cx_Oracle.connect("system", "pythonoracle", "localhost/XE")
+
+    def insertaNuevoTipo(self, nombre):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT MAX(ID) FROM PRO_TIPO")
+            maxID = cursor.fetchone()[0]
+            if maxID is None:
+                newID = 1
+            else:
+                newID = maxID + 1
+            cursor.execute("INSERT INTO PRO_TIPO (ID, NOMBRE) VALUES(:P1, UPPER(:P2))",(newID, nombre))
+        except self.connection.error as error:
+            print("Error: ", error)
+        return cursor
+
+
+    def listaDeTipos(self):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT ID, NOMBRE FROM PRO_TIPO")
+        except self.connection.error as error:
+            print("Error: ", error)
+        return cursor
+
+    # def encuentraLibroPorId(self):
+    #     cursor = self.connection.cursor()
+    #     try:
+    #     except self.connection.error as error:
+    #         print("Error: ", error)
+    #
+    #
+    #
+    #
+    # def modificaTipo(self):
+    #     print()
+    #
+    # def borraTipoPorId(self):
+    #     print()
