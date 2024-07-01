d1354279d98f2c05008ea04cf71ecd8753d09914
XainaDiGraft <obuentam@gmail.com>
2024-07-01 13:16:06 +0200

    Autor resuelto

--- proyPythonFinCurso/appFinCurso/urls.py
+++ proyPythonFinCurso/appFinCurso/urls.py
@@ -5,7 +5,11 @@ from appFinCurso import views
 urlpatterns=[
     path('',views.inicio,name='inicio'),
     path('inicio',views.inicio,name='inicio'),
-    path('viewAltaAutorF', views.altaAutorF,name='AltaAutorForm'),
-    path('altaAutorRes',views.altaAutorRes,name="altaAutorRes"),
-    path('viewAutores',views.autores,name='Autores')
+    path('viewAltaAutorF', views.altaAutorF, name='AltaAutorForm'),
+    path('altaAutorRes', views.altaAutorRes, name="altaAutorRes"),
+    path('viewAutores', views.autores, name='Autores'),
+    path('viewAutor', views.autor, name='Autor'),
+    path('borrAutor', views.delAutor, name='borrAutor'),
+    path('viewModAutor', views.modAutorF, name='viewModAutor'),
+    path('modAutorRes', views.modAutor, name='modAutorRes')
 ]
