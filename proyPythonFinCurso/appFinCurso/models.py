6b4d57da1de9d660115133a18a403999eb71aa4d
Ted Pincas <my.dev.challenges@gmail.com>
2024-07-01 10:07:12 +0200

    Update models.py
    quito código basura y lo dejo limpito con solo con 1 método

--- proyPythonFinCurso/appFinCurso/models.py
+++ proyPythonFinCurso/appFinCurso/models.py
@@ -4,21 +4,6 @@ class DatosTipo:
     def __init__(self):
         self.connection = cx_Oracle.connect("system", "pythonoracle", "localhost/XE")
 
-    def insertaNuevoTipo(self, nombre):
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("SELECT MAX(ID) FROM PRO_TIPO")
-            maxID = cursor.fetchone()[0]
-            if maxID is None:
-                newID = 1
-            else:
-                newID = maxID + 1
-            cursor.execute("INSERT INTO PRO_TIPO (ID, NOMBRE) VALUES(:P1, UPPER(:P2))",(newID, nombre))
-        except self.connection.error as error:
-            print("Error: ", error)
-        return cursor
-
-
     def listaDeTipos(self):
         cursor = self.connection.cursor()
         try:
@@ -26,18 +11,3 @@ class DatosTipo:
         except self.connection.error as error:
             print("Error: ", error)
         return cursor
-
-    # def encuentraLibroPorId(self):
-    #     cursor = self.connection.cursor()
-    #     try:
-    #     except self.connection.error as error:
-    #         print("Error: ", error)
-    #
-    #
-    #
-    #
-    # def modificaTipo(self):
-    #     print()
-    #
-    # def borraTipoPorId(self):
-    #     print()
